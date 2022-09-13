82fead545fc17d15462a8e67776684bc8c3212a1
Sungmin Choi <castlemin999@gmail.com>
2022-09-13 12:38:49 +0900

    feat(20220913): Add Transaction rollback

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
@@ -0,0 +1,169 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberRepositoryV2 {
+
+    private final DataSource dataSource;
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values(?, ?)";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, member.getMemberId());
+            stmt.setInt(2, member.getMoney());
+            stmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("error", e);
+            throw e;
+        } finally {
+            close(conn, stmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+
+        String sql = "select * from member where member_id = ?";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, memberId);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("No found Member : " + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("error", e);
+            throw e;
+        } finally {
+            close(conn, stmt, rs);
+        }
+    }
+
+    public Member findById(Connection conn, String memberId) throws SQLException {
+
+        String sql = "select * from member where member_id = ?";
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, memberId);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("No found Member : " + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("error", e);
+            throw e;
+        } finally {
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(stmt);
+        }
+    }
+
+    public int update(String memberId, int money) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        String sql = "update member set money = ? where member_id = ?";
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, money);
+            stmt.setString(2, memberId);
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error("error", e);
+            throw e;
+        } finally{
+            close(conn, stmt, null);
+        }
+    }
+
+    public int update(Connection conn, String memberId, int money) throws SQLException {
+        PreparedStatement stmt = null;
+
+        String sql = "update member set money = ? where member_id = ?";
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, money);
+            stmt.setString(2, memberId);
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error("error", e);
+            throw e;
+        } finally{
+            JdbcUtils.closeStatement(stmt);
+        }
+    }
+
+    public int delete(String memberId) throws SQLException{
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        String sql = "delete from member where member_id = ?";
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, memberId);
+            return stmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("error", e);
+            throw e;
+        } finally{
+            close(conn, stmt, null);
+        }
+    }
+
+    private void close(Connection conn, PreparedStatement stmt, ResultSet rs) throws SQLException {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        JdbcUtils.closeConnection(conn);
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection conn = dataSource.getConnection();
+        log.info("get connection={}, class={}", conn, conn.getClass());
+        return conn;
+    }
+
+}
